3287a9c95afe8ce426e8c02c67cd71e59d3475d7
Rajkumar <rajkumarkundu212@gmail.com>
2025-01-02 15:48:21 +0530

    update logoutUser controller

--- src/controllers/user.controler.js
+++ src/controllers/user.controler.js
@@ -162,8 +162,8 @@ const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: {
-        refrechToken: undefined,
+      $unset: {
+        refrechToken: 1,//this remove the field frm document
       },
     },
     {
